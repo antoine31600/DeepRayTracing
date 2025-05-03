59b8eca2c81d94734cbc0beb0f23fb926b09d5c2
antoine31600 <93413636+antoine31600@users.noreply.github.com>
2025-05-02 20:42:19 -0400

    Delete materials directory

--- materials/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-
-from .base import BaseMaterial
-from .ideal import IdealMaterial
-from .mirror import Mirror
-from .material_file import MaterialFile
-from .material import Material
-from .abbe import AbbeMaterial
